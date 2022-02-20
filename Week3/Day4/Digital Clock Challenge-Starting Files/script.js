8196160f7279bd849bc988a5d4da39dc0c843115
Ann-Margaret <amp1706@googlemail.com>
2022-02-20 20:42:20 +0000

    Push Digital clock
    Push Digital clock

---
+++ Week3/Day4/Digital Clock Challenge-Starting Files/script.js
@@ -0,0 +1,22 @@
+//Hints
+//function setTime() {
+
+let time = document.getElementById("time");
+
+function setTime() {
+
+    const clock = new Date();
+    let hours = clock.getHours();
+    let mins = clock.getMinutes();
+    let secs = clock.getSeconds();
+    time.innerHTML = hours + " " + mins + " " + secs;
+    setInterval(setTime, 1000);
+}
+
+
+setTime();
+
+
+//}
+//setTime();
+//
